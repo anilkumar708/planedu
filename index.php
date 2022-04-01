1feb6d6a19d9dc2c6af7ff141efa2f8f2df8844c
anilkumar708 <anilkumar1995708@gmail.com>
2022-04-01 16:23:02 +0530

    first trail

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+  	    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="assets/css/home.css">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+        <script src="assets/js/main.js"></script>
+
+    </head>
+    <header>
+        <div class="first-section">
+            <div class="container">
+                <div class="header-strip">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="row">
+                                <div class="col-lg-1">
+                                    <i class="fa fa-envelope social-style"></i>
+                                </div>
+                                <div class="col-lg-5">
+                                    <p class="para-1">info@planedu.in</p>
+                                </div>
+                                <div class="col-lg-1">
+                                    <i class="fa fa-mobile-phone social-style" style="font-size: 20px !important;"></i>
+                                </div>
+                                <div class="col-lg-5">
+                                    <p class="para-1">+91 95388 83336</p>
+                                </div>
+                            </div>   
+                        </div>
+                        <div class="col-lg-6"> 
+                            
+                        </div>
+                        <div class="col-lg-2 hidden-mobile">
+                            <a href="#">
+                                <i class="fa fa-facebook-f para-1 social-style"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa fa-twitter social-style"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa fa-linkedin social-style"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa fa-instagram social-style"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa fa-youtube social-style"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <nav class="navbar">
+                        <div class="container-fluid">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                    <span class="sr-only">Navigation ein-/ausblenden</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand hidden-sm hidden-md hidden-lg" href="#">
+                                    <img src="assets/img/home/logo.png">
+                                </a>
+                            </div>
+                            <div class="collapse navbar-collapse">
+                                <ul class="nav navbar-nav">
+                                    <li><a href="#">HOME</a></li>
+                                    <li><a href="#">ABOUT</a></li>
+                                    <li><a href="#">COURSES</a></li>
+                                    <li><a href="#">COLLEGES</a></li>
+                                    <li class="hidden-xs">
+                                        <a href="#"><img src="assets/img/home/logo.png"></a>
+                                    </li>
+                                    <li><a href="#">RESOURCES</a></li>
+                                    <li><a href="#">COMEDK</a></li>
+                                    <li><a href="#">CONTACT</a></li>
+                                    <li><a href="#">LOGIN</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+                <div class="home-cont-center home-space">
+                    <h1 style="font-weight: 700; letter-spacing:3px; font-size: 50px;">WELCOME TO <span>PLANEDU</span></h1><br><br>
+                    <p class="home-para">Planedu provides a comprehensive range of educational services for aspiring students who are seeking <br>to get expert career counseling, assist them in the right course selection, and educating them <br>about the admission procedures and application dates in reputed colleges</p>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <body>
+        <div class="home-bg1">
+            <div class="second-section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 home-cont">
+                            <h1>Why choose us?</h1>
+                        </div>
+                        <div class="col-lg-8 home-cont vl">
+                            <ul>
+                                <li>We are all ears to your interests and aspirations. On hearing, our adept Career Professionals will suggest suitable courses in top Colleges & Universities. </li><hr>
+
+                                <li>With 12 years of expertise, we bestow you with all the pros and cons, foresight, and future scope of your career options.</li><hr>
+
+                                <li>With our Career Connoisseurs on board, they pull you out from every confusion and set you on a clear path.</li><hr>
+
+                                <li>Exemplary Admission guidance and special mentoring for Entrance Exams.</li><hr>
+
+                                <li>We leave no stone unturned to make you land your dream career.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="home-service">
+                <div class="container">
+                    <div class="home-cont">
+                        <h1>Services</h1>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <img src="assets/img/home/advanced.png">
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="assets/img/home/spot.png">
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="assets/img/home/virtual.png">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <img src="assets/img/home/university.png">
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="assets/img/home/skill.png">
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="assets/img/home/academic.png">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div>
+                <div style="clear: both;">
+                    <div class="mission">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <img src="assets/img/home/mission.png" width="110%">
+                                </div>
+                                <div class="col-lg-8">
+                                    <h1 class="home-heading">Our Mission</h1>
+                                    <ul class="para-1">
+                                    <li>To create an ecosystem of profound education crafted to be delivered at a global stage and a trusted partner in higher education consultation.</li><br>
+                                    <li>To create a sustainable growth model for every university/college/institutions of higher education assisting them to achieve global education delivery standards at minimal effort.</li><br>
+                                    <li>To ensure the outcome of skilled and professional human resources worthy of delivering excellence in their career while driving them towards their right career choice.</li><br>
+                                    </ul>
+                                </div>
+                            </div>
+                            <h1 class="home-heading">Our Vision</h1>
+                            <p class="para-1">
+                                Our vision is to be the leading catalyst of implementation of an empowered & transformed education that delivers the most impactful and value-added learning for aspirants of higher education while everyone receives equal opportunities of global standard education at minimal efforts.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+
+            <div class="space">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <img src="assets/img/home/why-study.png" width="100%">
+                        </div>
+                        <div class="col-lg-7"><br><br><br><br>
+                            <h1 class="home-heading">Why Study In BANGALORE?</h1>
+                            <p class="para-1">When it comes to pursuing undergraduate and postgraduate courses, Bangalore is the top-rated city offering high-quality education in premium institutes. <br><br>A hub of top educational institutions with 20+ recognized universities is registered with PlanEdu to seek variant options. <br><br>This portal provides you with accurate information related to the approved and recognized colleges, institutes, and universities across Karnataka for all sorts of courses and educational programs.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="space">
+                <div class="mission">
+                    <div class="container">
+                        <h1 class="home-heading">Featured Top Universities</h1>
+                        <div class="container">
+	                        <div class="row">
+		                        <div class="MultiCarousel" data-items="1,2,3,4" data-slide="3" id="MultiCarousel"  data-interval="3000">
+                                    <div class="MultiCarousel-inner">
+                                        <div class="item">
+                                            <div class="pad15">
+                                                <img src="assets/img/home/ramaiah.png">
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <div class="pad15">
+                                                <img src="assets/img/home/swami.png">
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <div class="pad15">
+                                                <img src="assets/img/home/alliance.png">
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <div class="pad15">
+                                                <img src="assets/img/home/dypatil.png">
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <div class="pad15">
+                                                <img src="assets/img/home/cmr.png">
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <div class="pad15">
+                                                <img src="assets/img/home/reva.png">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <button class="btn btn-primary leftLst"><</button>
+                                    <button class="btn btn-primary rightLst">></button>
+                                </div>
+	                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body> 
+</html>
---
+++ templates/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>  	
+  	    <meta charset="utf-8">
+  	    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	    <title><?php echo $pgtitle ?></title>
+        <meta name="keywords" content="<?php echo $pgKeywords ?>">
+        <meta name="description" content="<?php echo $pgDesc ?>">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    </head>
+    <header>
+
+    </header>
+    <body>
+
+    </body>
+</html>
